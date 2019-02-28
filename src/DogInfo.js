5f36ff837112d95ab86175f49d6c413f2b171ce7
Jarrianm <jarrian.mclean@gmail.com>
2019-02-27 19:38:31 -0500

    Able to turn Bad Dogs into Good dog without refresh but not the other way around

--- src/DogInfo.js
+++ src/DogInfo.js
@@ -1,17 +1,48 @@
 import React from "react"
 
 const DogInfo = (props) => {
-console.log(props.changed.isGoodDog)
+console.log(props)
 		const imgStyle = {
 			boxShadow: `0 0 30px 15px yellow, 0 0 50px 15px yellow, 0 0 75px 45px yellow`
 		}
 
 		let clickHandler = (vale) =>{
-			console.log('clicked',props.dog)
+			console.log('clicked',props.changed)
 			props.onChange(props.dog)
 		}
 
 		return (
+			// props.dog.isGoodDog?
+			// (<div id="dog-info">
+			// 	<img src={props.dog.image} style={imgStyle} alt="PLEASE SELECT A PUP NAME"/>
+			//  	<h2>{props.dog.name}</h2>
+			//  	<button onClick={() => clickHandler()}>Good Dog</button>
+			// 	</div>)
+			// 	:
+			// 	(<div id="dog-info">
+			//  	<img src={props.dog.image} style={imgStyle} alt="PLEASE SELECT A PUP NAME"/>
+			// 	<h2>{props.dog.name}</h2>
+			//  	<button onClick={() => clickHandler()}>Bad Dog</button>
+			// 	 </div>)
+				 
+			// 	 props.changed.isGoodDog?
+				 
+			// 	 (<div id="dog-info">
+			// 	 <img src={props.changed.image} style={imgStyle} alt="PLEASE SELECT A PUP NAME"/>
+			// 	  <h2>{props.changed.name}</h2>
+			// 	  <button onClick={() => clickHandler()}>Good Dog</button>
+			// 	 </div>)
+			// 	 :
+			// 	 <div id="dog-info">
+			// 	 <img src={props.changed.image} style={imgStyle} alt="PLEASE SELECT A PUP NAME"/>
+			// 	  <h2>{props.chnaged.name}</h2>
+			// 	  <button onClick={() => clickHandler()}>Bad Dog</button>
+			// 	 </div>
+
+				 
+
+			
+			// 
 			props.changed.isGoodDog?
 			(<div id="dog-info">
 			<img src={props.changed.image} style={imgStyle} alt="pup pup"/>
@@ -19,22 +50,22 @@ console.log(props.changed.isGoodDog)
 			<button onClick={() => clickHandler()}>Good Dog</button>
 			</div>)
 			:
-			
-			(props.dog.isGoodDog ?(
+
+			props.dog.isGoodDog ?(
 			<div id="dog-info">
 				<img src={props.dog.image} style={imgStyle} alt="pup pup"/>
 				<h2>{props.dog.name}</h2>
-				<button onClick={() => clickHandler()}>Good Dog</button>
+				<button onClick={() => clickHandler()}>BAd Dog</button>
 				</div>)
 				:
 				(<div id="dog-info">
 				<img src={props.dog.image} style={imgStyle} alt="PLEASE SELECT A DOG FROM DOG BAR"/>
 				<h2>{props.dog.name}</h2>
-				<button onClick={() => clickHandler()}>Bad Dog</button>
+				<button onClick={() => clickHandler()}> Bad Dog</button>
 				</div>))
 				
 
-		)
+		
 
 }
 
